e090b3113f5539e1871fa0572f41e1405e421eda
amelink <amelink@Amelies-MacBook-Pro.local>
2017-05-22 18:57:07 -0700

    Last change for freecodecamp

--- Codewars entrainement.js
+++ Codewars entrainement.js
@@ -633,6 +633,43 @@ function sumArray(array) {
 
 
 
+Find Maximum and Minimum Values of a List
+
+Your task is to make two functions, max and min (maximum and minimum in PHP)
+that take a(n) array/vector of integers list as input and outputs,
+respectively, the largest and lowest number in that array/vector.
+// Solution
+var min = function(list){
+    var iMin = 0;
+    for (var i = 0; i < list.length; i += 1) {
+        if (list[i] < list[iMin]) {
+            iMin = i;
+        }
+    }
+    return list[iMin];
+}
+
+var max = function(list){
+    var iMax = 0
+    for (var i = 0; i < list.length; i++) {
+        if (list[i] > list[iMax]) {
+            iMax = i;
+        }
+    }
+
+// Autre Solution
+var min = function(list){
+    return Math.min(...list);
+}
+
+var max = function(list){
+    return Math.max(...list);
+}
+
+
+
+
+
 Last
 
 Find the last element of a list.
